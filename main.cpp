11c7ffb44fe4d28429da79bf0c7401afb2a5ba31
2021051615215HuangRuiXian <2395013885@qq.com>
2023-03-06 22:53:10 +0800

    copyversion

--- main.cpp
+++ main.cpp
@@ -1,9 +1,23 @@
 //author:huangruixian
-#include "finddialog.h"
+#include <QLabel>
+#include <QPushButton>
+#include <QApplication>
+#include <memory>
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    FindDialog dialog=new FindDialog;
-    dialog.show();
-    return a.exec();
+   QApplication app(argc,argv);
+
+   auto window=std::make_unique<QWidget>();
+   window->setGeometry(500,500,300,200);
+
+   auto label=new QLabel("hello qt",window.get());
+   label->setAlignment(Qt::AlignCenter);
+   label->setGeometry(100,100,80,30);
+
+   auto button=new QPushButton("Exit",window.get());
+   button->setGeometry(300,300,200,60);
+
+   QObject::connect(button,&QPushButton::clicked,&app,&QApplication::closeAllWindows);
+   window->show();
+   return app.exec();
 }
