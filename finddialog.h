8fe6731f2758feff8a35e7ad4fb15dcf56075a6d
2021051615215HuangRuiXian <2395013885@qq.com>
2023-03-06 22:36:53 +0800

    version 1.0

---
+++ finddialog.h
@@ -0,0 +1,48 @@
+//author:HuangRuiXian
+//data:2023.3.4
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <QDialog>
+#include <QCheckBox>
+#include <QPushButton>
+#include <QLineEdit>
+#include <QLabel>
+#include <QString>
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QApplication>
+
+class QCheckBox;
+class QPushButton;
+class QLineEdit;
+class QLabel;
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Dialog; }
+QT_END_NAMESPACE
+
+class FindDialog : public QDialog
+{
+    Q_OBJECT
+
+public:
+    FindDialog(QWidget *parent = nullptr);
+    ~FindDialog();
+signals:
+    void findNext(const QString &str,Qt::CaseSensitivity cs);
+    void findPrevious(const QString &str,Qt::CaseSensitivity cs);
+
+private slots:
+    void findClicked();
+    void enabledFindButton(const QString &text);
+private:
+    QLabel *label;
+    QLineEdit *lineEdit;
+    QCheckBox *caseCheckBox;
+    QCheckBox *backwardCheckBox;
+    QPushButton*findButton;
+    QPushButton*closeButton;
+
+};
+#endif // DIALOG_H
